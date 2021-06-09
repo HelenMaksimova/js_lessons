b65806c9b428b25c5ce9324af92bdf085544a8bd
Helen Maksimova <53541086+HelenMaksimova@users.noreply.github.com>
2021-06-09 23:48:55 +0300

    Add files via upload

---
+++ chess/task_05_01.js
@@ -0,0 +1,52 @@
+// Создать функцию, генерирующую шахматную доску. 
+// Можно использовать любые html-теги. Доска должна быть верно разлинована на черные и белые ячейки. 
+// Строки должны нумероваться числами от 1 до 8, столбцы — латинскими буквами A, B, C, D, E, F, G, H.
+
+function createChessBord() {
+
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+    let white = false;
+
+    let chessBoard = document.createElement('div');
+    chessBoard.className = 'chess';
+
+    for (let row = 0; row <= 8; row++) {
+
+        let currentRow = document.createElement('div');
+        currentRow.className = 'row';
+
+        for (let cell = 0; cell <= 8; cell++) {
+            let contentText;
+            let currentCell = document.createElement('div');
+            currentCell.className = 'cell';
+
+            if (row == 0) {
+                (cell == 0) ? currentCell.classList.add('upCell', 'leftCell') : currentCell.classList.add('upCell');
+                contentText = (cell == 0) ? '' : letters[cell - 1];
+            }
+            else {
+                if (cell == 0) {
+                    currentCell.classList.add('leftCell');
+                }
+                else {
+                    (white) ? currentCell.classList.add('whiteCell') : currentCell.classList.add('blackCell');
+                }
+                white = !white
+                contentText = (cell == 0) ? nums[row - 1] : '';
+            }
+
+            currentCell.textContent = contentText;
+            currentRow.appendChild(currentCell);
+
+        }
+
+        chessBoard.appendChild(currentRow);
+
+    }
+
+    document.querySelector('body').appendChild(chessBoard);
+
+}
+
+createChessBord()
